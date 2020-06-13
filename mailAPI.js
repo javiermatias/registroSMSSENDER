d98dc048bf2b50ac9073823a20fab4ae251628d4
javiermatias <javierjimenez78@gmail.com>
2020-06-13 07:11:31 -0300

    upload mail

--- mailAPI.js
+++ mailAPI.js
@@ -9,10 +9,25 @@ exports.sendEmail = function(req){
     headers: { 'Content-Type': 'application/json' },
     body: `{"from": "info@novanok.com", "to": "${process.env.toMail}", "subject": 
     "${req.body.nombre}  ${req.body.apellido}  ${req.body.email}", 
-    "body": "Un nuevo Cliente!", "webhook": "emoji"}`
+    "body": "Un nuevo Cliente sms!", "webhook": "emoji"}`
   }).then(response => {
     console.log( response);
   }).catch(err => { console.log(err); });
+
+}
+
+
+exports.sendEmailWhats = function(req){
+
+  fetch('http://34.94.120.97:5000/api/message/email', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: `{"from": "info@novanok.com", "to": "${process.env.toMail}", "subject": 
+  "${req.body.nombre}  ${req.body.apellido}  ${req.body.email}", 
+  "body": "Un nuevo Cliente Whatsapp!", "webhook": "emoji"}`
+}).then(response => {
+  console.log( response);
+}).catch(err => { console.log(err); });
 
 }
 
